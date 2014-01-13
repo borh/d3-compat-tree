8fcc041617c2d953e63bf6da321f7fdae9ecf4e2
Bor Hodošček <bor.hodoscek@gmail.com>
2014-01-13 22:46:43 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject d3-compat-tree "0.0.1"
+  :description "A d3 tree layout compatible data structure for Clojure using zippers"
+  :url "https://github.com/borh/d3-compat-tree.git"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [fast-zip "0.3.0"]
+                 [prismatic/plumbing "0.2.0"]]
+  :min-lein-version "2.0.0")
