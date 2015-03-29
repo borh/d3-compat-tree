c0c4e34cfaffe9162b7fa7c2c3ce76de952254e6
Bor Hodošček <bor.hodoscek@gmail.com>
2015-03-29 20:10:36 +0900

    updated to recent fast-zip and added schema annotations

--- src/d3_compat_tree/tree.clj
+++ src/d3_compat_tree/tree.clj
@@ -1,7 +1,9 @@
 (ns d3-compat-tree.tree
   (:require [fast-zip.core :as fz]
-            [plumbing.core :refer [for-map]])
-  (:import [fast_zip.core ZipperLocation]))
+            [plumbing.core :refer [for-map]]
+            [schema.core :as s])
+  (:import [fast_zip.core ZipperLocation ZipperPath]
+           (clojure.lang IFn)))
 
 ;; # Genre tree creation
 ;;
@@ -35,29 +37,76 @@
 ;; }
 ;; ```
 
-(defn tree-zipper [root]
+(s/defschema D3Tree
+  (s/maybe
+    {:name           s/Str
+     s/Keyword       s/Num
+     (s/optional-key :children) [(s/recursive #'D3Tree)]}))
+
+(s/defn tree-zipper :- ZipperLocation
+  [root :- {s/Keyword s/Any}]
   (letfn [(make-node [node children]
             (with-meta (assoc node :children (vec children)) (meta node)))]
     (fz/zipper map? :children make-node root)))
 
-(defn- root-loc-fz
+;; FIXME Workaround for vector usage where official fast-zip API expects an ISeq.
+(defn vector-down
+  "Returns the loc of the leftmost child of the node at this loc,
+  or nil if no children"
+  [^ZipperLocation loc]
+  (when (fz/branch? loc)
+    (when-let [cs (fz/children loc)]
+      (let [node (.-node loc), ^ZipperPath path (.-path loc)]
+        (ZipperLocation.
+          (.-ops loc)
+          (first cs)
+          (fz/ZipperPath.
+            []
+            (next ^clojure.lang.PersistentVector cs)
+            path
+            (if path (conj (.-pnodes path) node) [node])
+            nil))))))
+(defn vector-next
+  "Moves to the next loc in the hierarchy, depth-first. When reaching
+  the end, returns a distinguished loc detectable via end?. If already
+  at the end, stays there."
+  [^ZipperLocation loc]
+  (let [path (.-path loc)]
+    (if (identical? :end path)
+      loc
+      (or
+       (if (fz/branch? loc) (vector-down loc))
+       (fz/right loc)
+       (loop [p loc]
+         (if-let [u (fz/up p)]
+           (or (fz/right u) (recur u))
+           (ZipperLocation. (.-ops loc) (.-node p) :end)))))))
+
+;; END FIXME
+
+(s/defn root-loc-fz :- ZipperLocation
   "zips all the way up and returns the root node, reflecting any changes.
   Modified from fast-zip.core/root to return location over node."
-  [^ZipperLocation loc]
+  [loc :- ZipperLocation]
   (if (identical? :end (.path loc)) ;; FIXME type hint .path
     loc
     (let [p (fz/up loc)]
       (if p
         (recur p)
         loc))))
-(defn- find-loc [field-name root]
+(s/defn find-loc :- (s/maybe ZipperLocation)
+  [field-name :- (s/either s/Keyword s/Str)
+   root :- ZipperLocation]
   (loop [loc root]
     (if loc
       (if (= field-name (-> loc fz/node :name))
         loc
         (recur (fz/right loc))))))
 
-(defn- update-keys [m1 m2 key-fns]
+(s/defn update-keys :- {s/Keyword s/Any}
+  [m1 :- {s/Keyword s/Any}
+   m2 :- {s/Keyword s/Any}
+   key-fns :- IFn]
   (let [ks (set (keys key-fns))]
     (merge m1
            (for-map [k ks
@@ -65,36 +114,41 @@
                      :when (or v1 v2)]
                k ((k key-fns) v1 v2)))))
 
-(defn seq-to-tree
+(s/defn seq-to-tree :- D3Tree
   "Transforms a vector of hierarchies (just another vector) into a tree data structure suitable for export to JavaScript.
 
   The underlying algorithm utilizes a custom tree zipper function.
   One downside to using zippers here is that searching for the child nodes is linear, but since the tree is heavily branched, this should not pose a problem even with considerable data.
   TODO: sentence-level features Q/A, conversational, etc?"
-  [hierarchies & {:keys [merge-fns root-name root-values]
-                  :or {merge-fns {:count +}
-                       root-name "Genres"}}] ; -> [[:a :b :c :d] [:a :b :x :z] ... ]
-  (fz/root ; Return final tree.
-   (reduce ; Reduce over hierarchies.
-    (fn [tree m]
-      (->> (:genre m)
-           (reduce ; Reduce over fields in hierarchy (:a, :b, ...).
-            (fn [loc field]
-              (if-let [found-loc (if-let [child-loc (fz/down loc)] (find-loc field child-loc))]
-                (fz/edit found-loc update-keys m merge-fns #_merge-keys #_merge-fn) ; Node already exists: update counts in node.
-                (-> loc ; Add new node and move loc to it (insert-child is a prepend op).
-                    (fz/insert-child (merge {:name field}
-                                            (select-keys m (keys merge-fns))))
-                    fz/down)))
-            tree)
-           root-loc-fz)) ; Move to root location.
-    (tree-zipper (merge {:name root-name}
-                        (or root-values
-                            (for-map [[k v] merge-fns]
-                                k (reduce v (map k hierarchies)))))) ;; FIXME: seems this is overridden???
-    hierarchies)))
+  [hierarchies :- [{:genre [s/Str] s/Keyword s/Any}]
+   & options :- [{(s/optional-key :merge-fns)   {s/Keyword IFn}
+                  (s/optional-key :root-name)   s/Str
+                  (s/optional-key :root-values) {s/Keyword s/Any}}]]
+  (let [{:keys [merge-fns root-name root-values]
+         :or   {merge-fns {:count +}
+                root-name "Genres"}} (first options)] ; FIXME better destructuring.
+    (fz/root                                                ; Return final tree.
+      (reduce                                               ; Reduce over hierarchies.
+        (fn [tree m]
+          (->> (:genre m)
+               (reduce                                      ; Reduce over fields in hierarchy (:a, :b, ...).
+                 (fn [loc field]
+                   (if-let [found-loc (if-let [child-loc (vector-down loc)] (find-loc field child-loc))]
+                     (fz/edit found-loc update-keys m merge-fns #_merge-keys #_merge-fn) ; Node already exists: update counts in node.
+                     (vector-down                                  ; Add new node and move loc to it (insert-child is a prepend op).
+                       (fz/insert-child loc
+                                        (assoc (select-keys m (keys merge-fns))
+                                          :name field)))))
+                 tree)
+               root-loc-fz))                                ; Move to root location.
+        (tree-zipper (assoc (or root-values
+                                (for-map [[k v] merge-fns]
+                                  k (reduce v (map k hierarchies))))
+                       :name root-name))
+        hierarchies))))
 
-(defn tree-path [tree-loc]
+(s/defn tree-path :- (s/either [s/Str] [s/Keyword])
+  [tree-loc :- ZipperLocation]
   (loop [path '()
          loc tree-loc]
     (if loc
@@ -103,7 +157,10 @@
        (fz/up loc))
       (into [] path))))
 
-(defn- get-count-in-tree [field loc ks]
+(s/defn get-count-in-tree :- s/Num
+  [field :- (s/either s/Keyword s/Str)
+   loc :- ZipperLocation
+   ks :- [s/Str]]
   (loop [node-names ks
          t-loc loc
          count nil]
@@ -112,21 +169,29 @@
       (if t-loc
         (if-let [found-loc (find-loc (first node-names) t-loc)]
           (recur (next node-names)
-                 (fz/down found-loc)
-                 (-> found-loc fz/node field)))))))
+                 (vector-down found-loc)
+                 (-> found-loc fz/node field))
+          0)
+        0))))
 
-(defn normalize-tree
+(s/defn normalize-tree :- D3Tree
   "Given a genre tree in-tree, returns normalized counts by genre, which are passed in as norm-tree.
   The type of normalized counts returned depend on in-tree, while norm-tree can use a number of counts: document, paragraph and sentence."
-  [norm-tree in-tree & {:keys [update-field update-fn boost-factor clean-up-fn]
-                        :or {update-field :count boost-factor 1000000 update-fn /}}]
-  (let [in-tree-zipper (tree-zipper in-tree)
+  [norm-tree :- D3Tree
+   in-tree   :- D3Tree
+   & options :- [{(s/optional-key :update-field) s/Keyword
+                  (s/optional-key :update-fn)    IFn
+                  (s/optional-key :boost-factor) s/Num
+                  (s/optional-key :clean-up-fn)  IFn}]]
+  (let [{:keys [update-field update-fn boost-factor clean-up-fn]
+         :or {update-field :count boost-factor 1000000 update-fn /}} (first options) ; FIXME better destructuring.
+        in-tree-zipper (tree-zipper in-tree)
         norm-tree-zipper (tree-zipper norm-tree)]
     (loop [loc in-tree-zipper]
       (if (fz/end? loc)
         (fz/root loc)
         (recur
-         (fz/next ; NOTE: get-count-in-tree should always return a positive number.
+         (vector-next ; NOTE: get-count-in-tree should always return a positive number.
           (fz/edit loc
                    update-in
                    [update-field]
