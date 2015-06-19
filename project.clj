c619f48b8e652bbc1919d34b09c13f47194a355d
Bor Hodošček <bor.hodoscek@gmail.com>
2015-06-19 18:05:04 +0900

    updated deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/borh/d3-compat-tree.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [fast-zip "0.6.1" :exclusions [com.cemerick/austin]]
-                 [prismatic/plumbing "0.4.1"]
-                 [prismatic/schema "0.4.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}}
+                 [prismatic/plumbing "0.4.4"]
+                 [prismatic/schema "0.4.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.8.0-alpha3"]]}}
   :min-lein-version "2.0.0")
